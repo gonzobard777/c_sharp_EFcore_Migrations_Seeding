51d2b903d8c80a9ff0c8583e3cb7b8b6d0c30ccd
gonzobard777 <gonzo.bard@dwfe.ru>
2025-03-30 20:33:49 +0300

    .

---
+++ Infrastructure/Migrations/20250330173045_AdminIsActive.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminIsActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "User");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "User",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "User");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Salt",
+                table: "User",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
